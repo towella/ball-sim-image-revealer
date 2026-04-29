a79a514e57300d187b9002a86cd7005cb6418495
towella <114838654+towella@users.noreply.github.com>
2026-04-29 16:35:53 +1000

    Switched to different circle-circle collision solution which now works as intended. However as circles come to rest collisions cease to function??

--- source/ball.hpp
+++ source/ball.hpp
@@ -18,7 +18,7 @@ class Ball {
 
             radius = random.between(5, 21);
             pos = position;
-            vel = Point2D(random.between(-10, 10), random.between(-10, 10));
+            vel = Point2D(random.between(-10, 10), 0);
             mass = radius;
         }
 
@@ -65,29 +65,54 @@ class Ball {
             }
         }
 
-        // https://ericleong.me/research/circle-circle/
         void BallCollision(std::vector<Ball>& balls, Point2D& newPos) {
             for (Ball& ball : balls) {
-                double distanceSqrd = newPos.getDistanceNoSqrt(ball.pos);
+                //double distanceSqrd = newPos.getDistanceNoSqrt(ball.pos);
+                Point2D directionVector = Point2D(pos.x() - ball.pos.x(), pos.y() - ball.pos.y());
+                double distanceSqrd = directionVector.x() * directionVector.x() + directionVector.y() * directionVector.y();
                 double combinedRadiiSqrd = (radius + ball.radius) * (radius + ball.radius);
                 // ball not self and is overlapping
                 if (&ball != this && distanceSqrd < combinedRadiiSqrd) {
+                    // https://gamedev.stackexchange.com/questions/121693/simulating-elastic-ball-collisions-quickly-escalates-to-disaster
+                    // https://codepen.io/anon/pen/QNXObe?editors=1010
+                    Point2D combinedVelocityVec = Point2D(ball.vel.x() - vel.x(), ball.vel.y() - vel.y());
+                    double dotProduct = combinedVelocityVec.x() * directionVector.x() + combinedVelocityVec.y() * directionVector.y();
+                    // 1. if dot product of two vectors is negative, then the angle between the vectors is greater 
+                    //    than 90 degrees;
+                    // 
+                    // 2. if the velocity vector is pointing away from the direction of the collision, then the 
+                    //    balls are moving away from each other;
+                    //
+                    // 3. if the balls are moving away from each other, then they are not colliding.
+                    if (dotProduct > 0) {
+                        double totalMass = mass + ball.mass;
+                        double massFactorOne = 2 * ball.mass / totalMass;
+                        double massFactorTwo = 2 * mass / totalMass;
+                        double scaledMomentum = dotProduct / distanceSqrd;
+                        double scalarOne = massFactorOne * scaledMomentum;
+                        double scalarTwo = massFactorTwo * scaledMomentum;
+                        vel.set(vel.x() + (scalarOne * directionVector.x()),
+                                vel.y() + (scalarOne * directionVector.y()));
+                        ball.vel.set(ball.vel.x() - (scalarTwo * directionVector.x()),
+                                     ball.vel.y() - (scalarTwo * directionVector.y()));
+                    }
 
-                    // this ball collides with other ball 
-                    double pushBack = sqrt(combinedRadiiSqrd - distanceSqrd);
-                    double angle = newPos.getAngleToRad(pos);  // angle to push point back along move vector
-                    newPos.set(ball.pos.x() + sin(angle) * pushBack,
-                               ball.pos.y() + cos(angle) * pushBack);
+                    // https://ericleong.me/research/circle-circle/
+                    // // this ball collides with other ball 
+                    // double pushBack = sqrt(combinedRadiiSqrd - distanceSqrd);
+                    // double angle = newPos.getAngleToRad(pos);  // angle to push point back along move vector
+                    // newPos.set(ball.pos.x() + sin(angle) * pushBack,
+                    //            ball.pos.y() + cos(angle) * pushBack);
 
-                    // this ball has velocity change from collision
-                    double distance = newPos.getDistance(ball.pos);
-                    Point2D normVecBallToSelf = Point2D((ball.pos.x() - pos.x()) / distance,
-                                                        (ball.pos.y() - pos.y()) / distance);
-                    double p = 2.0 * (double)(vel.x() * normVecBallToSelf.x() + vel.y() * normVecBallToSelf.y() - ball.vel.x() * normVecBallToSelf.x() - ball.vel.y() * normVecBallToSelf.y()) / (double)(mass + ball.mass);
-                    vel.set(vel.x() - p * mass * normVecBallToSelf.x(),
-                            vel.y() - p * mass * normVecBallToSelf.y());
-                    ball.vel.set(ball.vel.x() + p * ball.mass * normVecBallToSelf.x(),
-                                 ball.vel.y() + p * ball.mass * normVecBallToSelf.y());
+                    // // this ball has velocity change from collision
+                    // double distance = newPos.getDistance(ball.pos);
+                    // Point2D normVecBallToSelf = Point2D((ball.pos.x() - pos.x()) / distance,
+                    //                                     (ball.pos.y() - pos.y()) / distance);
+                    // double p = 2.0 * (double)(vel.x() * normVecBallToSelf.x() + vel.y() * normVecBallToSelf.y() - ball.vel.x() * normVecBallToSelf.x() - ball.vel.y() * normVecBallToSelf.y()) / (double)(mass + ball.mass);
+                    // vel.set(vel.x() - p * mass * normVecBallToSelf.x(),
+                    //         vel.y() - p * mass * normVecBallToSelf.y());
+                    // ball.vel.set(ball.vel.x() + p * ball.mass * normVecBallToSelf.x(),
+                    //              ball.vel.y() + p * ball.mass * normVecBallToSelf.y());
                 }
             }
         }
